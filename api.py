98da8659ef65333a9db3ce41880c319260496db6
diwifibeachjacket <diwifibeachjacket@gmail.com>
2025-01-07 03:59:33 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -1,32 +1,5 @@
-from statsmodels.distributions.copula.copulas import (
-    CopulaDistribution)
-
-from statsmodels.distributions.copula.archimedean import (
-    ArchimedeanCopula, FrankCopula, ClaytonCopula, GumbelCopula)
-import statsmodels.distributions.copula.transforms as transforms
-
-from statsmodels.distributions.copula.elliptical import (
-    GaussianCopula, StudentTCopula)
-
-from statsmodels.distributions.copula.extreme_value import (
-    ExtremeValueCopula)
-import statsmodels.distributions.copula.depfunc_ev as depfunc_ev
-
-from statsmodels.distributions.copula.other_copulas import (
-    IndependenceCopula, rvs_kernel)
-
-
-__all__ = [
-    "ArchimedeanCopula",
-    "ClaytonCopula",
-    "CopulaDistribution",
-    "ExtremeValueCopula",
-    "FrankCopula",
-    "GaussianCopula",
-    "GumbelCopula",
-    "IndependenceCopula",
-    "StudentTCopula",
-    "depfunc_ev",
-    "transforms",
-    "rvs_kernel"
-]
+__all__ = ["TLinearModel", "PoissonGMLE", "PoissonOffsetGMLE", "PoissonZiGMLE"]
+from .tmodel import TLinearModel
+from .count import (PoissonGMLE, PoissonOffsetGMLE, PoissonZiGMLE,
+                    #NonlinearDeltaCov
+                    )
